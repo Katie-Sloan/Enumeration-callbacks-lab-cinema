03e58d438082d44facfad10ad5ffd0822da6fac1
Katie Sloan <katie.a.sloan@gmail.com>
2020-11-18 14:57:05 +0000

    second commit

--- models/cinema.js
+++ models/cinema.js
@@ -2,4 +2,10 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+Cinema.prototype.titleList = function () {
+  return this.films.map((film) => {
+    return film.title; 
+  });
+}
+
 module.exports = Cinema;
